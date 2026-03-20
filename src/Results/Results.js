3a7a9e1ee3c4f5e1c07b0dbb5c0ae7b1bfbf53b8
Jeff Henrichs <jeffhenrichs37@gmail.com>
2026-03-19 21:52:29 -0400

    Added Tracklist to Results.js

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,9 +1,11 @@
 import "./Results.css";
+import Tracklist from "../Tracklist/Tracklist";
 
 const Results = (props) => {
   return (
     <div className="results_container">
       <h2>Results</h2>
+      <Tracklist />
     </div>
   );
 };
--- src/Track/Track.js
+++ src/Track/Track.js
@@ -3,7 +3,7 @@ import "./Track.css";
 export default function Track() {
   return (
     <div className="track_container">
-      <h3>Tracks goes here</h3>
+      <h5>Insert Song here</h5>
     </div>
   );
 }
