b8c46fe66e2bfa10047c37d3ce9f844ce4b55aed
Jeff Henrichs <jeffhenrichs37@gmail.com>
2026-04-03 22:19:13 -0400

    Removed music icon from Track data

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -1,13 +1,8 @@
 import "./Track.css";
-import { FaPlayCircle, FaPauseCircle } from "react-icons/fa";
 
 export default function Track(props) {
   return (
     <div className="track_container">
-      <FaPlayCircle
-        className="music_icon"
-        onClick={() => console.log(`Playing: ${props.songName}`)}
-      />
       <div className="track_info">
         <h3 className="song_name">{props.songName}</h3>
         <p className="artist_info">
